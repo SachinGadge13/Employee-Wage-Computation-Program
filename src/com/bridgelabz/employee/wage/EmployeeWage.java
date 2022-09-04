9870da0c96b6a593ebdcb7b22700dd844973a300
SachinGadge13 <gadges14@gmail.com>
2022-09-04 14:25:27 +0530

    Added: Employee partTime salary program

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -9,14 +9,18 @@ public class EmployeeWage {
 
 		int wagePerHour = 20;
 		int fullDayHours = 8;
+		int halfDayHours = 4;
 		int salary = 0;
 
 		Random random = new Random();
-		int randomNum = random.nextInt(2); // 0-1
+		int randomNum = random.nextInt(3); // 0-1-2
 
 		if (randomNum == 1) {
 			System.out.println("Employee is present");
 			salary = wagePerHour * fullDayHours;
+		} else if (randomNum == 2) {
+			System.out.println("Employee worked for half day");
+			salary = wagePerHour * halfDayHours;
 		} else
 			System.out.println("Employee is absent");
 		System.out.println("Employee salary is : " + salary);
