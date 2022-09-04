697f0337dd7ea96515755747d98869f817103fc4
SachinGadge13 <gadges14@gmail.com>
2022-09-04 15:06:23 +0530

    Added: Employee Monthly Salary program

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -11,24 +11,31 @@ public class EmployeeWage {
 		int fullDayHours = 8;
 		int halfDayHours = 4;
 		int salary = 0;
+		int workingDayPerMonth = 20;
+		int monthlySalary = 0;
+		
+		for (int i = 1; i <= workingDayPerMonth; i++) {
+			Random random = new Random();
+			int randomNum = random.nextInt(3); // 0-1-2
 
-		Random random = new Random();
-		int randomNum = random.nextInt(3); // 0-1-2
+			switch (randomNum) {
+			case 0:
+				System.out.println("Employee is absent");
+				break;
+			case 1:
+				System.out.println("Employee is present");
+				salary = wagePerHour * fullDayHours;
 
-		switch (randomNum) {
-		case 0:
-			System.out.println("Employee is absent");
-			break;
-		case 1:
-			System.out.println("Employee is present");
-			salary = wagePerHour * fullDayHours;
-			break;
-		case 2:
-			System.out.println("Employee worked for half day");
-			salary = wagePerHour * halfDayHours;
-			break;
+				break;
+			case 2:
+				System.out.println("Employee worked for half day");
+				salary = wagePerHour * halfDayHours;
+				break;
+			}
+			monthlySalary = monthlySalary + salary;
 		}
-		System.out.println("Employee salary is : " + salary);
+		System.out.println("Employee Monthly salary is : " + monthlySalary);
+
 	}
 
 }
