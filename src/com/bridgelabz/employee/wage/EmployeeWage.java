a5bfb047328c3ea108a034028cbd2141f5697306
SachinGadge13 <gadges14@gmail.com>
2022-09-04 13:50:23 +0530

    Added: Employee is present/absent program

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.employee.wage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Random random = new Random();
+		int randomNum = random.nextInt(2); // 0-1
+
+		if (randomNum == 1)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+
 	}
 
 }
