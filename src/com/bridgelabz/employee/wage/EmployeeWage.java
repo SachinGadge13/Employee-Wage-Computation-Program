9cd68c42183e0b3b4515b8149b5849e670b5f0d9
SachinGadge13 <gadges14@gmail.com>
2022-09-04 14:08:29 +0530

    Added: Employee full-time salary program

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -6,14 +6,20 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int salary = 0;
+
 		Random random = new Random();
 		int randomNum = random.nextInt(2); // 0-1
 
-		if (randomNum == 1)
+		if (randomNum == 1) {
 			System.out.println("Employee is present");
-		else
+			salary = wagePerHour * fullDayHours;
+		} else
 			System.out.println("Employee is absent");
-
+		System.out.println("Employee salary is : " + salary);
 	}
 
 }
